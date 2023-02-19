5756f9142f71a65c61704ff29dc036429c8728fe
Fadzai <audreymusodza@gmail.com>
2023-02-19 13:23:43 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int i = 48;
+
+	int x = 97;
+
+	while (i <= 57)
+
+	{
+		putchar(i);
+
+		i++;
+
+
+	}
+
+	while (x <= 102)
+
+	{
+		putchar(x);
+
+		x++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
+
+
