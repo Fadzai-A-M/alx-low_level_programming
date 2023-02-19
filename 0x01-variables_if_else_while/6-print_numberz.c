6279d8bcf55a54b8130056023b12dc37cb6175ec
Fadzai <audreymusodza@gmail.com>
2023-02-19 10:15:36 -0800

    0-9 again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int x = 48;
+
+	while (x <= 57)
+
+	{
+		putchar(x);
+
+		x++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
+
