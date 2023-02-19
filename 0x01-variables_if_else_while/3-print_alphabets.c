a6e2ddb1264f26f38c424b59d4f57b1ad899e381
Fadzai <audreymusodza@gmail.com>
2023-02-19 04:53:25 -0800

    A-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int i = 97;
+
+	while (i <= 122)
+
+	{
+		putchar(i);
+
+		i++
+	}
+
+	int x = 65;
+
+	while (x <= 90)
+
+	{
+		putchar(x);
+
+		x++;
+
+	}
+
+	{
+		putchar('\n')
+
+	}
+
+	return (0);
+
+}
