20fd7c930a5e49e72b8bb86094b7d73bc68d8912
Fadzai <audreymusodza@gmail.com>
2023-02-21 14:40:14 -0800

    print the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	int i;
+
+	i = 97;
+
+	while (i <= 122)
+
+	{
+		_putchar(i);
+
+			i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 0. Write a program that prints _putchar, followed by a new line.
+1. 1-alphabet.c
