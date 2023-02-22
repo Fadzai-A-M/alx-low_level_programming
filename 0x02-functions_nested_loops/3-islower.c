8597a81278c0bd2e1fd318f3e43903f1cb16445e
Fadzai <audreymusodza@gmail.com>
2023-02-22 08:54:52 -0800

    t

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,11 +11,29 @@
 int _islower(int c)
 
 {
-	int c[] = {111, 108, 99, 73, 115, 70, 20, 85, 110};
+	int c;
 	
-	_islower(c);
+	c = _islower('o');
+
+	_putchar(c + '0');
+
+	c = _islower(108);
+
+	_puthcar(c + '0');
+
+	c = _islower('c');
+
+	_putchar(c + '0');
+
+	c = _islower('I');
+
+	_putchar(c + '0');
+
+	c = _islower('s');
+
+	_putchar(c + 0);
 
 
-	return (1);
+	return (0);
 
 }
