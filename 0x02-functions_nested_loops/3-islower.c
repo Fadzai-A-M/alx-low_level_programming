b3af7d8196fc645e340bf4113a270d0332a238a5
Fadzai <audreymusodza@gmail.com>
2023-02-21 18:43:29 -0800

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * int _islower  -  Writes a function that checks for lowercase character
+ *
+ *@c: the character to check
+ *
+ * Return: 1 if lower case (Success)
+ */
+int _islower(int c);
+
+{
+	if (islower(c))
+
+	{
+		return (1);
+	}
+
+	else
+
+	{
+		return (0);
+
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0. Write a program that prints _putchar, followed by a new line.
 1. 1-alphabet.c
 2. Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+3. Write a function that checks for lowercase character.
