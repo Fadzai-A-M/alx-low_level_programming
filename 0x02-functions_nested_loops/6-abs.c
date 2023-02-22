c61c17d19a2ded34d01237a6cf9a42e15aa5ebba
Fadzai <audreymusodza@gmail.com>
2023-02-22 12:13:33 -0800

    _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @int: value to be delcared
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int)
+
+{
+	int i;
+
+	if (i < 0)
+
+	{
+		return (-i);
+
+	}
+
+	if (i > 0)
+
+	{
+		return (i);
+
+	}
+
+}
