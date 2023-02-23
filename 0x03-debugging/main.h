16b7f150055dd10e28732d4440183fbd2ae76283
Fadzai <audreymusodza@gmail.com>
2023-02-23 09:46:00 -0800

    argest of three integers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,11 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+
 printf("Infinite loop incoming :(\n");
+
 int largest_number(int a, int b, int c);
+
+void print_remaining_days(int month, int day, int year);
 
 #endif
