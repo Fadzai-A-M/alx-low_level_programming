bf28d778b2ed26c976826dc6e38a431c9e95a6c3
Fadzai <audreymusodza@gmail.com>
2023-02-22 16:24:38 -0800

    times tables

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <time>
+
+/**
+ * jack_bauer -  prints every minute of the day of Jack Bauer.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void)
+
+{
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+
+{
+	int a;
+	int result;
+
+	for (a = 1; a <= 10; a++)
+
+	{
+		result = 9 * a;
+
+		_putchar(result + '0');
+
+	}
+
+	return (0);
+}
